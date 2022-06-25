637c5f2170d7e1de18bfd31157032f3321e58292
Giselle Chacon Nessi <gisecn@gmail.com>
2022-06-26 01:03:29 +0200

    solana pay set up

---
+++ README.md
@@ -0,0 +1,14 @@
+# buildspace Store on Solana Pay Project
+
+### **Welcome 👋**
+
+To get started with this project, clone this repo and follow these commands:
+
+1. Run `npm install` at the root of your directory
+2. Run `npm run dev` to start the project
+3. Start coding!
+
+### Mainnet <--> Dev
+
+1. usdcAddress in createTransaction.js
+2. network in \_app.js, createTransaction.js
---
+++ pages/index.js
@@ -0,0 +1,106 @@
+import React, { useEffect, useState } from "react";
+import CreateProduct from "../components/CreateProduct";
+import Product from "../components/Product";
+import HeadComponent from "../components/Head";
+// import { PublicKey } from "@solana/web3.js";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+
+// Constants
+const TWITTER_HANDLE = "Gi_Chacon";
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+const App = () => {
+  // This will fetch the users' public key (wallet address) from any wallet we support
+  const { publicKey } = useWallet();
+  const isOwner = publicKey
+    ? publicKey.toString() === process.env.NEXT_PUBLIC_OWNER_PUBLIC_KEY
+    : false;
+  const [creating, setCreating] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  const renderNotConnectedContainer = () => (
+    <div>
+      <img
+        className="connect-img"
+        src="https://media2.giphy.com/media/NKvWjoiESiENO/giphy.gif?cid=ecf05e473iejjxcrxml8ble0pisyl4nc863xn2o419id4krj&rid=giphy.gif&ct=g"
+        alt="tupac"
+        
+      />
+      <div className="button-container">
+        <WalletMultiButton className="cta-button connect-wallet-button" />
+      </div>
+    </div>
+  );
+
+  useEffect(() => {
+    if (publicKey) {
+      fetch(`/api/fetchProducts`)
+        .then((response) => response.json())
+        .then((data) => {
+          setProducts(data);
+          console.log("Products", data);
+        });
+    }
+  }, [publicKey]);
+
+  const renderItemBuyContainer = () => (
+    <div className="products-container">
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
+    </div>
+  );
+
+  return (
+    <div className="App">
+      <HeadComponent />
+      <div className="container">
+        <header className="header-container">
+          <p className="header"> Gi Store </p>
+          <p className="sub-text"> All eyez on me </p>
+
+          {/* Make form public  */}
+          {/* {isOwner && (
+            <button
+              className="create-product-button"
+              onClick={() => setCreating(!creating)}
+            >
+              {creating ? "Close" : "Create Product"}
+            </button>
+          )} */}
+
+          {publicKey && (
+            <button
+              className="create-product-button"
+              onClick={() => setCreating(!creating)}
+            >
+              {creating ? "Close" : "Create Product"}
+            </button>
+          )}
+        </header>
+
+        <main>
+          {creating && <CreateProduct />}
+          {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
+        </main>
+
+        <div className="footer-container">
+          <img
+            alt="Twitter Logo"
+            className="twitter-logo"
+            src="twitter-logo.svg"
+          />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built on @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
